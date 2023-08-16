0f7e0eb2a20272a4b21577973ea82d1bafb50904
akurochka <andrii.kurochka@zorallabs.com>
2023-08-16 14:36:26 +0300

    ci: Fix CI

--- bench/Benchmarks.cs
+++ bench/Benchmarks.cs
@@ -1,4 +1,7 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿using System.IO;
+using System.Threading.Tasks;
+
+using BenchmarkDotNet.Attributes;
 
 using OpaDotNet.Compilation.Cli;
 using OpaDotNet.Compilation.Interop;
