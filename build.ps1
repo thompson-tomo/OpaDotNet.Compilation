ebea5a8bf6ef0a1bf61ca685c8bc96d664d478a7
akurochka <andrii.kurochka@zorallabs.com>
2023-08-16 16:25:30 +0300

    ci: Improve build script

--- build.ps1
+++ build.ps1
@@ -32,9 +32,8 @@ $targets | %{
     $env:CC = $_.CC
     $env:CXX = $_.CXX
 
-    $env:WSLENV = "GOOS/u:GOARCH/u:CGO_ENABLED/u:CC/u:CXX/u"
-
     if ($IsWindows) {
+        $env:WSLENV = "GOOS/u:GOARCH/u:CGO_ENABLED/u:CC/u:CXX/u"
         wsl /usr/local/go/bin/go build -C ./interop -ldflags "-w -s" -buildmode=c-shared -o "../bin/$outPath/Opa.Interop.$($_.Ext)" ./main.go
     } else {
         go build -C ./interop -ldflags "-w -s" -buildmode=c-shared -o "../bin/$outPath/Opa.Interop.$($_.Ext)" ./main.go
